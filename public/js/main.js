f182769c8ac2f891817b882d6592f64f1e28c3a7
Joe Waine <joseph@comradeagency.com>
2017-03-15 23:18:06 -0700

    form fix

--- public/js/main.js
+++ public/js/main.js
@@ -233,63 +233,29 @@ window.onscroll = function(e) {
 
 
 
+// form
+$('#ajax-contact').submit(function(ev) {
+    // Prevent the form from actually submitting
+    ev.preventDefault();
 
-// contact form
+    // Get the post data
+    var data = $(this).serialize();
+
+    // Send it to the server
+    $.post('/', data, function(response) {
+        if (response.success) {
+
+alert('success');
+
+        } else {
+            // response.error will be an object containing any validation errors that occurred, indexed by field name
+            // e.g. response.error.fromName => ['From Name is required']
+            alert('An error occurred. Please try again.');
+
+alert('no success');
 
 
-$(function() {
-
-    // Get the form.
-    var form = $('#ajax-contact');
-
-    // Get the messages div.
-    var formMessages = $('#form-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function(e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-        // Serialize the form data.
-        var formData = $(form).serialize();
-
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        })
-        .done(function(response) {
-            // Make sure that the formMessages div has the 'success' class.
-            $(formMessages).removeClass('error');
-            $(formMessages).addClass('success');
-
-            // Set the message text.
-            $(formMessages).text(response);
-
-            // Clear the form.
-            $('#name').val('');
-            $('#email').val('');
-            $('#message').val('');
-        })
-        .fail(function(data) {
-            // Make sure that the formMessages div has the 'error' class.
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
-
-            // Set the message text.
-            if (data.responseText !== '') {
-                $(formMessages).text(data.responseText);
-            } else {
-                $(formMessages).text('Oops! An error occured and your message could not be sent.');
-            }
-        });
-
+        }
     });
-
 });
 
-
-
-
-// contact form
