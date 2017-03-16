da6ffde1c7a7f60deea239681c0ac1842d8f14d9
Joe Waine <joseph@comradeagency.com>
2017-03-15 22:58:55 -0700

    events

--- craft/templates/mailer.php
+++ craft/templates/mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "joe.waine@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
