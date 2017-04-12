1ad426781fe9e1f2a43f55532d50f6de0b071400
Joe Waine <joseph@comradeagency.com>
2017-04-12 10:13:56 -0700

    images

--- public/js/main.js
+++ public/js/main.js
@@ -538,11 +538,9 @@ window.open('http://www.opentable.com/single.aspx?rid=295054&restref=295054', '_
 
 });
 
-// $('.find-panel').prepend('<div class="address"><a href="https://goo.gl/maps/gZLnSb6DpV12" target="_blank">' + $('.address').text() + '<a></div>');
 
 
-
-$('.find-header').click(function(){
+$('.find-header, .find-panel .address').click(function(){
 
 window.open('https://goo.gl/maps/gZLnSb6DpV12', '_blank');
 
