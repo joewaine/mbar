27393629314168375c2513259119473ab9db1937
Joe Waine <joseph@comradeagency.com>
2017-06-14 12:32:11 +0000

    item edit

--- public/js/main.js
+++ public/js/main.js
@@ -664,3 +664,14 @@ $(window).resize(function () {
 });
 
 
+
+$('.contact-item-text.left').prepend('<div class="added-item-res">reserve<br>online</div>');
+
+
+
+$('.contact-item-text.for-tel').prepend('<div class="added-item-ph"><a href="tel:+12064578287">206 457 8287</div>')
+
+
+
+
+
