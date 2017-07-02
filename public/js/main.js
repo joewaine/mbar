7a294788e9d7384742f025f8b662b337c2b86cbf
Joe Waine <joseph@comradeagency.com>
2017-07-02 15:03:51 -0400

    hellotest

--- public/js/main.js
+++ public/js/main.js
@@ -689,4 +689,4 @@ $('.contact-item-text.left').prepend('<div class="added-item-res">reserve<br>onl
 $('.contact-item-text.for-tel').prepend('<div class="added-item-ph"><a href="tel:+12064578287">206 457 8287</div>')
 
 
-
+console.log('hellotest');
